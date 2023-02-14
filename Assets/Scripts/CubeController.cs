fc25d54c47749747324d3164d0710a98e4d9a662
BogDog4 <Bramanofficial@gmail.com>
2023-02-13 19:58:58 -0500

    make the colliders work

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -12,15 +12,20 @@ public class CubeController : MonoBehaviour
     public float zRange = 10.0f;
     public float recoilspeed = 2.0f;
 
+    public bool shootinterval = true;
+
     public GameObject projectileUp;
     public GameObject projectileDown;
     public GameObject projectileLeft;
     public GameObject projectileRight;
 
+    private Rigidbody rigidplayercube;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //tell it what rigid body is to fix the drifting issue
+        rigidplayercube = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -32,8 +37,13 @@ public class CubeController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
-        //checking if the player’s position is within ranges
+        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
         {
+            rigidplayercube.velocity = Vector3.zero;
+        }
+
+        //checking if the playerï¿½s position is within ranges
+        /*{
             if (transform.position.x < -xRange)
             { transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); }
             if (transform.position.x > xRange)
@@ -45,26 +55,55 @@ public class CubeController : MonoBehaviour
             { transform.position = new Vector3(transform.position.x, transform.position.y, zRange); }
 
         }
+        */
         //Projectile scripts and pushback
-        if (Input.GetKey(KeyCode.UpArrow))
+        //cooldown established
+        if (Input.GetKey(KeyCode.UpArrow)&&shootinterval)
         {
             transform.Translate(Vector3.back * Time.deltaTime * recoilspeed);
             Instantiate(projectileUp, transform.position, projectileUp.transform.rotation);
+            shootinterval = false;
+            StartCoroutine(cooldown());
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow)&&shootinterval)
         {
             transform.Translate(Vector3.forward * Time.deltaTime * recoilspeed);
             Instantiate(projectileDown, transform.position, projectileDown.transform.rotation);
+            shootinterval = false;
+            StartCoroutine(cooldown());
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow)&&shootinterval)
         {
             transform.Translate(Vector3.right * Time.deltaTime * recoilspeed);
             Instantiate(projectileLeft, transform.position, projectileLeft.transform.rotation);
+            shootinterval = false;
+            StartCoroutine(cooldown());
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow)&&shootinterval)
         {
             transform.Translate(Vector3.left * Time.deltaTime * recoilspeed);
             Instantiate(projectileRight, transform.position, projectileRight.transform.rotation);
+            shootinterval = false;
+            StartCoroutine(cooldown());
+
         }
+        
+        //i just made a function
+        //
+    }
+    IEnumerator cooldown() 
+    {
+yield return new WaitForSeconds(0.25f);
+shootinterval = true;
+    }
+    //collision function
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("lava"))
+        {
+            Destroy(gameObject);
+            Debug.Log("collided");
+        }
+        
     }
 }
